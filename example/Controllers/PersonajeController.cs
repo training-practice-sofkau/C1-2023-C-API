ac53372a4dc91289337d503bbac6d57a8d0ef52c
JuanJaramilloSalazar <salazarjdj@gmail.com>
2023-01-26 16:31:45 -0500

    Estructura inicial y funcion GET
    Se crea inicialmente el proyecto realizando la creacion de la estructura creando las carpetas Models y Controller con sus respectivas clases implementando el metodo inicial GET a modo de prueba

---
+++ example/Controllers/PersonajeController.cs
@@ -0,0 +1,39 @@
+﻿using example.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+
+namespace example.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class PersonajeController : ControllerBase
+	{
+
+		private static readonly string[] Habilidades = new[]
+		{
+			"Fuerza", "Psiquico", "Velocidad", "Volar", "Invisibilidad", "Cyborg"
+		};
+
+		private readonly ILogger<PersonajeController> _logger;
+
+		public PersonajeController(ILogger<PersonajeController> logger)
+		{
+			_logger = logger;
+		}
+
+		[HttpGet(Name = "GetPersonaje")]
+		public IEnumerable<Personaje> Get()
+		{
+			return Enumerable.Range(1, 5).Select(index => new Personaje
+			{
+				ID = Random.Shared.Next(0, 100),
+				Name = "Personaje "+index,
+				Ability = Habilidades[Random.Shared.Next(Habilidades.Length)],
+				Power = Random.Shared.Next(0, 1000),
+				Health = Random.Shared.Next(0, 1000),
+			})
+			.ToArray();
+		}
+
+	}
+}
---
+++ example/Models/Personaje.cs
@@ -0,0 +1,16 @@
+﻿namespace example.Models
+{
+	public class Personaje
+	{
+		public int ID { get; set; }
+
+		public string Name { get; set; }
+
+		public string Ability { get; set; }
+
+		public int Power { get; set; }
+
+		public int Health { get; set;}
+
+	}
+}
