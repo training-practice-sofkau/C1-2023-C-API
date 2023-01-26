33f62a31b5b3c4a010993df67036d0a49aa0e580
ESTEVAN TANGARIFE CORREA <teban1928@gmail.com>
2023-01-26 17:07:53 -0500

    Creacion
    Se realizo creacion de la clase programador en el archivo modelo

---
+++ example/Models/Programmer.cs
@@ -0,0 +1,15 @@
+﻿namespace example.Models
+{
+    public class Programmer
+    {
+        
+        public Guid Id { get; set; }
+
+        public string CompleteName { get; set; }
+
+        public string Email { get; set; }
+
+        public long PhoneNumber { get; set; }
+
+    }
+}
