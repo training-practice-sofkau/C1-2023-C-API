0d867969d04507c9c04ea531e850b1102bed1e2e
Javier Carrillo <javiercamur98@gmail.com>
2023-01-27 12:51:20 -0500

    Se genera el controlador de la tabla Producto

--- example/Models/ProductsdbContext.cs
+++ example/Models/ProductsdbContext.cs
@@ -17,7 +17,7 @@ public partial class ProductsdbContext : DbContext
 
     public virtual DbSet<Product> Products { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder);
 //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
   //      => optionsBuilder.UseSqlServer("Server=DESKTOP-35I32D9;Database=PRODUCTSDB;Integrated Security=True; TrustServerCertificate=True;");
 
