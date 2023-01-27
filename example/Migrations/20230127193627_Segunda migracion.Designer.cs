f6a5f05087f61fde30ec0dfcc11f98f8c0c3d9dd
Jesus <jesusdavidpoloespitia@gmail.com>
2023-01-27 14:41:01 -0500

    Segunda migracion a la base de Datos
    Se ha agregado un campo a la DB llamado estado para realizar el DELETE de tipo logico

--- example/Migrations/20230127193627_Segunda migracion.Designer.cs
+++ example/Migrations/20230127193627_Segunda migracion.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using example.Data;
 
@@ -11,9 +12,11 @@ using example.Data;
 namespace example.Migrations
 {
     [DbContext(typeof(MascotasAPIDbContext))]
-    partial class MascotasAPIDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230127193627_Segunda migracion")]
+    partial class Segundamigracion
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
