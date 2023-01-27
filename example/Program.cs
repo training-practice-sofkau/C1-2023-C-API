83fe3de67715747dbf5fcbf07fd975be7b806a16
Javier Carrillo <javiercamur98@gmail.com>
2023-01-27 12:39:45 -0500

    Se establece la conexion a la base de datos y se crea el modelo

--- example/Program.cs
+++ example/Program.cs
@@ -1,3 +1,7 @@
+using example.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ProductsdbContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("conexion")));
+
 
 var app = builder.Build();
 
