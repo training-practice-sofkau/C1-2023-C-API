211a5360192f9eb1156a40347f251322e0cd5a22
Rolando <rolandoantoniohe@ufps.edu.co>
2023-01-27 07:55:59 -0500

    Clase model, con sus atributos y excepciones

---
+++ example/Model/Player.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace example.Model
+{
+    public class Player
+    {
+        public int id { get; set; }
+        [RegularExpression("^[1-9]{1}[0-9]{7}$",
+            ErrorMessage = "Ingresar DNI sin puntos")]
+
+        public string? name { get; set; }
+        [RegularExpression("^[a-zA-Z]{1,25}$",
+           ErrorMessage = "Solo se permiten letras en este campo y debe contener entre 1 y 25 caracteres sin espacios")]
+
+        public string? seleccionClub { get; set; }
+        [RegularExpression("^[a-zA-Z]{1,25}$",
+           ErrorMessage = "Solo se permiten letras en este campo y debe contener entre 1 y 25 caracteres sin espacios")]
+
+        public string? posicion { get; set; }
+        [RegularExpression("^[a-zA-Z]{1,25}$",
+           ErrorMessage = "Solo se permiten letras en este campo y debe contener entre 1 y 25 caracteres sin espacios")]        
+
+        public string? paisNacimiento { get; set; }
+        
+    }
+}
