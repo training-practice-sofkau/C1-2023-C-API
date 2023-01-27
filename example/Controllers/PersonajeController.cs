29ecc223f1da2ce8d9e6a5962dc63d959490ba87
JuanJaramilloSalazar <salazarjdj@gmail.com>
2023-01-26 21:49:15 -0500

    Metodo PUT ControllerPersonaje
    Se implementa el metodo PUT en la clase ControllerPersonaje

--- example/Controllers/PersonajeController.cs
+++ example/Controllers/PersonajeController.cs
@@ -40,5 +40,21 @@ namespace example.Controllers
 			await dbContext.SaveChangesAsync();
 			return Ok();
 		}
+
+		[HttpPut]
+		public async Task<Object> Put(Personaje personajeData)
+		{
+			if (personajeData == null || personajeData.ID == 0)
+				return BadRequest();
+
+			var product = await dbContext.Personajes.FindAsync(personajeData.ID);
+			if (product == null)
+				return NotFound();
+			product.Name = personajeData.Name;
+			product.Ability = personajeData.Ability;
+			product.Power = personajeData.Power;
+			await dbContext.SaveChangesAsync();
+			return Ok();
+		}
 	}
 }
