79524357fcfb2e29670b5c5b5e78a8293996caa6
ESTEVAN TANGARIFE CORREA <teban1928@gmail.com>
2023-01-27 12:17:04 -0500

    Implementacion
    Se implemento los mensajes de los errores posibles por el usuario y el delete logico

--- example/Controllers/ProgrammerController.cs
+++ example/Controllers/ProgrammerController.cs
@@ -2,20 +2,22 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Net;
 
 namespace example.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/Usuarios")]
     [ApiController]
     public class ProgrammerController : ControllerBase
     {
 
         private readonly ProgrammerContext _dbContext;
+        private Programmer pro = new Programmer();
+
 
 
         public ProgrammerController(ProgrammerContext dbContext)
         {
-
 
             _dbContext = dbContext;
 
@@ -23,15 +25,21 @@ namespace example.Controllers
 
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Programmer>>> GetProgrammer() {
+        public async Task<ActionResult<IEnumerable<Programmer>>>GetProgrammer() {
 
-            if (_dbContext == null) {
+            if (_dbContext == null)
+            {
 
-                return NotFound();
+                return BadRequest();
+
             }
 
 
-            return await _dbContext.Programmers.ToListAsync();
+            var activeRecords = _dbContext.Programmers.Where(r => r.IsActive != 0).ToList();
+
+            // return await _dbContext.Programmers.ToListAsync();
+
+            return  activeRecords;
 
         }
 
@@ -40,6 +48,8 @@ namespace example.Controllers
         public async Task<ActionResult<Programmer>> GetProgrammer(int id)
         {
 
+
+
             if (_dbContext == null)
             {
 
@@ -47,13 +57,13 @@ namespace example.Controllers
             }
 
             var programmer = await _dbContext.Programmers.FindAsync(id);
-            if (programmer==null)
+            if (programmer == null)
             {
                 return NotFound();
 
             }
-            
-            
+
+
             return programmer;
 
         }
@@ -62,12 +72,35 @@ namespace example.Controllers
         [HttpPost]
         public async Task<ActionResult<Programmer>> PostProgrammer(Programmer programmer) {
 
+            if (string.IsNullOrWhiteSpace(programmer.CompleteName))
+            {
+
+
+                return BadRequest(new
+                {
+                    code = 400,
+                    message = "El nombre es un dato requerido no dejar en blanco por favor"
+                }) ;
+                
+               
+            }
+
+            if (string.IsNullOrWhiteSpace(programmer.Email))
+            {
+
+
+                return BadRequest(new
+                {
+                    code = 400,
+                    message = "El correo electronico es un dato requerido no dejar en blanco por favor"
+                });
+
+            }
+
             _dbContext.Programmers.Add(programmer);
             await _dbContext.SaveChangesAsync();
 
             return CreatedAtAction(nameof(GetProgrammer), new { id = programmer.Id }, programmer);
-        
-        
         
         }
 
@@ -78,19 +111,15 @@ namespace example.Controllers
             if (id != programmer.Id)
             {
 
-                return BadRequest();
+               // return HttpStatusCodeResult( "");
 
             }
 
-
             _dbContext.Entry(programmer).State = EntityState.Modified;
-
 
             try
             {
-
                 await _dbContext.SaveChangesAsync();
-
 
             }
             catch (DbUpdateConcurrencyException)
@@ -98,32 +127,23 @@ namespace example.Controllers
 
                     if (!ProgrammerAvailable(id))
                     {
-
                         return NotFound();
-
-
                     }
                     else {
-
 
                         throw;
                     
                     }
 
-               
-
-
             }
 
             return Ok();
-
 
         }
 
 
         [HttpDelete("{id}")]
         public async Task<ActionResult> DeleteProgrammer(int id) {
-
 
             if (_dbContext.Programmers == null){
 
@@ -133,28 +153,34 @@ namespace example.Controllers
 
             }
 
-            var programmer = await _dbContext.Programmers.FindAsync(id);
+          //  var program = await _dbContext.Programmers.FindAsync(id);
            
-            if (programmer == null)
+           /* if (program == null)
             {
 
                 return NotFound();
 
             }
-            _dbContext.Programmers.Remove(programmer);
+           */
 
-            await _dbContext.SaveChangesAsync();
+            var recordToUpdate = _dbContext.Programmers.FirstOrDefault(r => r.Id == id);
 
+            if (recordToUpdate != null )
+            {
+
+                recordToUpdate.IsActive = 0;
+                _dbContext.SaveChanges();
+
+
+            }
+
+            //
+           // _dbContext.Programmers.Remove(program);
+
+           // await _dbContext.SaveChangesAsync();
 
             return Ok();        
         }
-
-
-
-
-
-
-
 
 
         private bool ProgrammerAvailable(int id)
