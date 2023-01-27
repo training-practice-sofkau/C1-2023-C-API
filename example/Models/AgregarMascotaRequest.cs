dbf79808c030ddaa3cf8f4940b75289340642b97
Jesus <jesusdavidpoloespitia@gmail.com>
2023-01-27 11:32:05 -0500

    Validacion de campos vacios
    No sepueden registrar campos vacios

--- example/Models/AgregarMascotaRequest.cs
+++ example/Models/AgregarMascotaRequest.cs
@@ -1,12 +1,20 @@
-﻿namespace example.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace example.Models
 {
     public class AgregarMascotaRequest
-    {
-        public string NombreDeLaMascota { get; set; }
-        public string TipoDeMascota { get; set; }
-        public string NombreDelTutor { get; set; }
-        public string CorreoDelTutor { get; set; }
-        public long Celular { get; set; }
-        public string Direccion { get; set; }
+    { 
+            [Required]
+            public string NombreDeLaMascota { get; set; }
+            [Required]
+            public string TipoDeMascota { get; set; }
+            [Required]
+            public string NombreDelTutor { get; set; }
+            [Required]  
+            public string CorreoDelTutor { get; set; }
+            [Required]
+            public long Celular { get; set; }
+            [Required]
+            public string Direccion { get; set; }   
     }
 }
