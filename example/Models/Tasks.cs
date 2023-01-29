efe398e7f1f701b0fe89da92557c7e6a1cd6f665
Kevin Baquero <baquerochavarro@gmail.com>
2023-01-29 15:25:37 -0500

    Adding Tasks Model

---
+++ example/Models/Tasks.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace tasks.Models
+{
+    public class Tasks
+    {
+        [Required]
+        public Guid Id { get; set; }   
+        [Required]
+        public string TaskName { get; set; }
+        [Required]
+        public string TaskDescription { get; set; }
+        [Required]
+        public string CreatedBy { get; set; }
+        [Required]
+        public string Priority { get; set; }
+    }
+}
