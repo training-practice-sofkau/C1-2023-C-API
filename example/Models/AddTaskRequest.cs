037d51e9ce860cf67a56922554f49be52971a84b
Kevin Baquero <baquerochavarro@gmail.com>
2023-01-29 15:27:05 -0500

    Updating Tasks Model
    Adding Model to request add tasks

---
+++ example/Models/AddTaskRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace tasks.Models
+{
+    public class AddTaskRequest
+    {
+        [Required]
+        public string TaskName { get; set; }
+        [Required]
+        public string TaskDescription { get; set; }
+        [Required]
+        public string CreatedBy { get; set; }
+        [Required]
+        public string Priority { get; set; }
+    }
+}
