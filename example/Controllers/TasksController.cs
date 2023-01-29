9db27e957f16ff70b6ef5ccf03c155fc75d27f21
Kevin Baquero <baquerochavarro@gmail.com>
2023-01-29 15:41:34 -0500

    Adding Task Controller
    Adding GetAllTasks Method

---
+++ example/Controllers/TasksController.cs
@@ -0,0 +1,26 @@
+﻿using tasks.Models;
+using tasks.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace tasks.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TasksController : Controller
+    {
+        private readonly TasksAPIDbContext dbContext;
+
+        public TasksController(TasksAPIDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet(Name = "GetAllTasks")]
+        public async Task<IActionResult> GetAllTasks()
+        {
+            return Ok(await dbContext.Tasks.ToListAsync());
+        }
+
+    }
+}
