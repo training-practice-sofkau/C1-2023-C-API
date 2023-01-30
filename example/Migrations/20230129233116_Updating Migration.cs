904460a69ab139ade3860bbc5f5bfda083dff93c
Kevin Baquero <baquerochavarro@gmail.com>
2023-01-29 19:30:38 -0500

    Adding migrations
    Adding migrations for DB integration

---
+++ example/Migrations/20230129233116_Updating Migration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace tasks.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatingMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "State",
+                table: "Tasks",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "State",
+                table: "Tasks");
+        }
+    }
+}
